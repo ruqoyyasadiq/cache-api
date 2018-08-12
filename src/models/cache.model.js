8c5ed1d6806a142d5f23f40c14a2970bdb59cdb7
Rukayat Sadiq <fruqoyya@gmail.com>
2018-08-12 18:58:04 +0100

    add pre-hook to check number of entries before saving

--- src/models/cache.model.js
+++ src/models/cache.model.js
@@ -14,9 +14,10 @@ const cacheSchema = new Schema({
   },
   ttl: {
     type: Number,
+    default: 1440,
     required: true
   },
-  created_on: {
+  created_at: {
     type: Date,
     default: Date.now,
     required: true
@@ -24,4 +25,18 @@ const cacheSchema = new Schema({
 })
 
 const Cache = mongoose.model('Cache', cacheSchema)
+
+// Before saving, check if total entries is equal to max entries, if it is fetch all entries with sort in desc, delete oldest value and pass control to next value
+
+cacheSchema.prependListener('save', (next) => {
+  Cache.count({}, (err, count) => {
+    const maxCacheEntries = process.env.MAX_CACHE_ENTRIES
+    if(count > maxCacheEntries) {
+      Cache.find({}, {}, { sort: { 'created_at': -1 } }, (err, result) => {
+        // Add logic to remove oldest value before moving to next
+      })
+    }
+    next
+  })
+})
 module.exports = Cache;
