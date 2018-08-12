6ce3ad1723f16d80847114af451af5276eea8496
Rukayat Sadiq <fruqoyya@gmail.com>
2018-08-12 15:00:16 +0100

    spin up sserver

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+dotenv.config();
+
+const environment = process.env.NODE_ENV
+
+
+// connect to mongoDB
+mongoose.connect(process.env.DB_URL);
+mongoose.connection.on('connected', () => {
+  console.info('Opened connection to mongo db');
+});
+
+const app = express();
+
+if (environment === 'development') {
+  app.use(logger('dev'));
+}
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Fetch port value from env var or use default
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.info(`App started on port ${port} on ${environment} environment`)
+})
